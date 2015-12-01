ad2ea9f972d8cee3b7c77864def92837eeda48db
Antoine Le Calvez <antoine@alc.io>
2015-12-01 18:52:23 -0500

    fixed licence

--- blockchain_parser/__init__.py
+++ blockchain_parser/__init__.py
@@ -5,8 +5,8 @@
 # It is subject to the license terms in the LICENSE file found in the top-level
 # directory of this distribution.
 #
-# No part of python-bitcoinlib, including this file, may be copied, modified,
-# propagated, or distributed except according to the terms contained in the
-# LICENSE file.
+# No part of bitcoin-blockchain-parser, including this file, may be copied,
+# modified, propagated, or distributed except according to the terms contained
+# in the LICENSE file.
 
 __version__ = "0.0.1"
--- blockchain_parser/blockchain.py
+++ blockchain_parser/blockchain.py
@@ -5,9 +5,9 @@
 # It is subject to the license terms in the LICENSE file found in the top-level
 # directory of this distribution.
 #
-# No part of python-bitcoinlib, including this file, may be copied, modified,
-# propagated, or distributed except according to the terms contained in the
-# LICENSE file.
+# No part of bitcoin-blockchain-parser, including this file, may be copied,
+# modified, propagated, or distributed except according to the terms contained
+# in the LICENSE file.
 
 import os
 import mmap
