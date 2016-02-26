2c6d4018a379c20870fa91cf3e0e19e7bc9bf480
Antoine Le Calvez <antoine@alc.io>
2016-02-26 21:24:58 +0000

    Change URL and add classifiers

--- setup.py
+++ setup.py
@@ -6,9 +6,16 @@ setup(
     name='blockchain-parser',
     version=__version__,
     packages=find_packages(),
-    url='http://p2sh.info/parser',
+    url='https://github.com/alecalve/python-bitcoin-blockchain-parser',
     author='Antoine Le Calvez',
     author_email='antoine@p2sh.info',
     description='Bitcoin blockchain parser',
-    test_suite='blockchain_parser.tests'
+    test_suite='blockchain_parser.tests',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
+        'Topic :: Software Development :: Libraries',
+    ]
 )
